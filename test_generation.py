9454effe4c674df566e5b9873722167910231ea7
lanji <jxl152@gmail.com>
2022-12-16 20:41:36 +0100

    evaluate unconditional shape generation (chain): run test_generation.py

--- test_generation.py
+++ test_generation.py
@@ -533,7 +533,7 @@ def main(opt):
 def parse_args():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dataroot', default='ShapeNetCore.v2.PC15k/')
+    parser.add_argument('--dataroot', default='data/ShapeNetCore.v2.PC15k/')
     parser.add_argument('--category', default='chair')
 
     parser.add_argument('--batch_size', type=int, default=50, help='input batch size')
